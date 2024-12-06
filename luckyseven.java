591be93d652a163f6eaf7f226da533fa68de5a88
hema11920 <kasireddygarihemasree@gmail.com>
2024-12-06 12:41:26 +0530

    Add files via upload

---
+++ luckyseven.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class luckyseven
+{
+	public static void main (String[] args) 
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String S=sc.next();
+		System.out.println(S.charAt(6));
+	}
+}
